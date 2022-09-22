89c7212ce8273f76ca9cef6b5124eed85a78ebd0
Pedro Henrique Longo <61807977+phlongo@users.noreply.github.com>
2022-09-21 22:31:26 -0300

    atividade_04

--- atividade_01/src/App.js
+++ atividade_01/src/App.js
@@ -1,91 +1,138 @@
-import './App.css';
-// import Evento from './components/Props';
-// import TipoIngresso from './components/Props_Ingressos';
-// import logo from './components/logo.svg';
-import React, { useState } from "react";
-  
-const App = () => {
+import React, { useEffect, useState } from "react";
 
-  //  Counter is a state initialized to 0
-  const [counter, setCounter] = useState(0)
-  
-  // Function is called everytime increment button is clicked
-  const handleClick1 = () => {
-    // Counter state is incremented
-    var valor = document.getElementById("counter").value;
-    setCounter(counter + valor++ )
-  }
-  
-  // Function is called everytime decrement button is clicked
-  const handleClick2 = () => {
-    // Counter state is decremented
-    var valor = document.getElementById("counter").value;
-    setCounter(counter - valor)
-  }
+function Atividade04() {
 
-  const handleClick3 = () => {
-    // Counter state is decremented
-    setCounter(0)
-  }
-  
-  return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-      fontSize: '300%',
-      position: 'absolute',
-      width: '100%',
-      height: '100%',
-      top: '-15%',
-    }}>
-      Counter App
-      <div style={{
-        fontSize: '120%',
-        position: 'relative',
-        top: '10vh',
-      }}>
-        {counter}<br/>
-        Counter: <input type="text" id="counter" name="nome" /><br />
-      </div>
-      <div className="buttons">
-        <button style={{
-          fontSize: '60%',
-          position: 'relative',
-          top: '20vh',
-          marginRight: '10px',
-          backgroundColor: 'green',
-          borderRadius: '8%',
-          color: 'white',
-        }}
-          onClick={handleClick1}>Increment</button>
-        <button style={{
-          fontSize: '60%',
-          position: 'relative',
-          top: '20vh',
-          marginLeft: '10px',
-          backgroundColor: 'red',
-          borderRadius: '8%',
-          color: 'white',
-        }}
-          onClick={handleClick2}>Decrement</button>
-        <button style={{
-          fontSize: '60%',
-          position: 'relative',
-          top: '20vh',
-          marginRight: '10px',
-          backgroundColor: 'blue',
-          borderRadius: '8%',
-          color: 'white',
-        }}
-          onClick={handleClick3}>Reset</button>
-      </div>
-    </div>
-  )
+    const [vetor, setVetor] = useState([]);
+    const [status, setStatus] = useState('Carregando');
+
+    useEffect(() => {
+      obterDados();
+    });
+
+    const obterDados = async () => {
+
+        const dados = await fetch('https://api.github.com/orgs/rocketseat/repos');
+        const converter = await dados.json();
+        setVetor(converter);
+        setStatus('>>> Dados de repositorio carregados <<<')
+
+    }
+
+
+    return(
+        <div>
+          <h1>{status}</h1>
+
+          
+            {vetor.map(objeto => (<header>
+            <h2>{objeto.name}</h2>
+            <p>{objeto.description}</p>
+            <a href = {objeto.html_url}> Acessar repositorio </a>
+            </header>))}
+
+          
+        </div>
+    );
+
 }
+
+export default Atividade04;
+
+
+
+
+
+
+
+
+// import './App.css';
+// // import Evento from './components/Props';
+// // import TipoIngresso from './components/Props_Ingressos';
+// // import logo from './components/logo.svg';
+// import React, { useState } from "react";
   
-export default App
+// const App = () => {
+
+//   //  Counter is a state initialized to 0
+//   const [counter, setCounter] = useState(0)
+  
+//   // Function is called everytime increment button is clicked
+//   const handleClick1 = () => {
+//     // Counter state is incremented
+//     var valor = document.getElementById("counter").value;
+//     setCounter(counter + valor++ )
+//   }
+  
+//   // Function is called everytime decrement button is clicked
+//   const handleClick2 = () => {
+//     // Counter state is decremented
+//     var valor = document.getElementById("counter").value;
+//     setCounter(counter - valor)
+//   }
+
+//   const handleClick3 = () => {
+//     // Counter state is decremented
+//     setCounter(0)
+//   }
+  
+//   return (
+//     <div style={{
+//       display: 'flex',
+//       flexDirection: 'column',
+//       alignItems: 'center',
+//       justifyContent: 'center',
+//       fontSize: '300%',
+//       position: 'absolute',
+//       width: '100%',
+//       height: '100%',
+//       top: '-15%',
+//     }}>
+//       Counter App
+//       <div style={{
+//         fontSize: '120%',
+//         position: 'relative',
+//         top: '10vh',
+//       }}>
+//         {counter}<br/>
+//         Counter: <input type="text" id="counter" name="nome" /><br />
+//       </div>
+//       <div className="buttons">
+//         <button style={{
+//           fontSize: '60%',
+//           position: 'relative',
+//           top: '20vh',
+//           marginRight: '10px',
+//           backgroundColor: 'green',
+//           borderRadius: '8%',
+//           color: 'white',
+//         }}
+//           onClick={handleClick1}>Increment</button>
+//         <button style={{
+//           fontSize: '60%',
+//           position: 'relative',
+//           top: '20vh',
+//           marginLeft: '10px',
+//           backgroundColor: 'red',
+//           borderRadius: '8%',
+//           color: 'white',
+//         }}
+//           onClick={handleClick2}>Decrement</button>
+//         <button style={{
+//           fontSize: '60%',
+//           position: 'relative',
+//           top: '20vh',
+//           marginRight: '10px',
+//           backgroundColor: 'blue',
+//           borderRadius: '8%',
+//           color: 'white',
+//         }}
+//           onClick={handleClick3}>Reset</button>
+//       </div>
+//     </div>
+//   )
+// }
+  
+// export default App
 
 // function App() {
 //   return(
